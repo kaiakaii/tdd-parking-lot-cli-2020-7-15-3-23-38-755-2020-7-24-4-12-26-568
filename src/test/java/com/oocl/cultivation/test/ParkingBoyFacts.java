2b415f86557a8d9c25972a9ab6ccda8c46bfa1be
SUNSK <senkk512@163.com>
2020-07-24 15:26:24 +0800

    [ParkLot][Sky] feature: add should_carTicket_when_park_given_car test

--- src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
@@ -1,10 +1,21 @@
 package com.oocl.cultivation.test;
 
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.CarTicket;
+import com.oocl.cultivation.ParkingLot;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ParkingBoyFacts {
     @Test
-    void write_your_first_test() {
-
+    void should_carTicket_when_park_given_car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        //when
+        CarTicket carTicket = parkingLot.park(car);
+        //then
+        assertNotNull(carTicket);
     }
 }
