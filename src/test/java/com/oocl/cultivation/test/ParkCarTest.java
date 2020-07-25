fcea83f360442d8abc74a2d340e8d865c916df5b
sky <senkk512@163.com>
2020-07-25 10:37:10 +0800

    [ParkCar][Sky] feature: add  should_return_park_car_in_next_parklot_position_when_park_given_full_parking_space test

--- src/test/java/com/oocl/cultivation/test/ParkCarTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkCarTest.java
@@ -24,14 +24,14 @@ public class ParkCarTest {
     }
     @Test
     void should_return_park_car_in_next_parklot_position_when_park_given_full_parking_space() {
-//        //given
-//        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot(10));
-//        for (int i = 0; i < 11; i++) {
-//            parkingBoy.park(new Car());
-//        }
-//        //when
-//        CarTicket carTicket = parkingBoy.park(new Car());
-//        //then
-//        assertNotNull(carTicket);
+        //given
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot(10));
+        for (int i = 0; i < 11; i++) {
+            parkingBoy.park(new Car());
+        }
+        //when
+        CarTicket carTicket = parkingBoy.park(new Car());
+        //then
+        assertNotNull(carTicket);
     }
 }
