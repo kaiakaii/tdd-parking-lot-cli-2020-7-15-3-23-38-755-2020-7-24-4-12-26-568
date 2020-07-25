cbb9122dc8243fd706e1448b665501036ad5925b
sky <senkk512@163.com>
2020-07-25 10:23:56 +0800

    [ParkCar][Sky] feature: add should_return_not_enough_position_when_park_given_full_parking_space test

--- src/test/java/com/oocl/cultivation/test/ParkCarTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkCarTest.java
@@ -1,9 +1,24 @@
 package com.oocl.cultivation.test;
 
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.CarTicket;
+import com.oocl.cultivation.ParkingBoy;
+import com.oocl.cultivation.ParkingLot;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParkCarTest {
     @Test
-    void name() {
+    void should_return_not_enough_position_when_park_given_full_parking_space() {
+        //given
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot(10));
+        for (int i = 0; i < 11; i++) {
+            parkingBoy.park(new Car());
+        }
+        //when
+        CarTicket carTicket = parkingBoy.park(new Car());
+        //then
+        assertEquals("Not enough position.",parkingBoy.getMessage());
     }
 }
